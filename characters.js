401d8c2f3b90badcf8c45acdebea53c0baef5730
Miki <57331129+TheFoxInBoots@users.noreply.github.com>
2025-03-28 17:38:39 +0100

    testing stuff

---
+++ characters.js
@@ -0,0 +1,28 @@
+const characters = {    
+    anders: {
+        name: "Anders",
+        avatar_url: "https://static.wikia.nocookie.net/dareckoning/images/6/6e/Anders.jpg/",
+    },
+    bull: {
+        name: "The Iron Bull",
+        avatar_url: "https://i.pinimg.com/736x/80/38/7d/80387df2e6602d68fd8fd313275b7e7a.jpg",
+    },
+    cullen: {
+        name: "Cullen Rutherford",
+        avatar_url: "https://i.pinimg.com/736x/bb/90/fe/bb90fef8d7def3cdeac4f87fdba6a932.jpg",
+    },
+    dorian: {
+        name: "Dorian Pavus",
+        avatar_url: "https://i.pinimg.com/736x/08/6c/67/086c673a40b64f8efecd9d4009b578ed.jpg",
+    },
+    solas: {
+        name: "Solas",
+        avatar_url: "https://64.media.tumblr.com/1f4e3bbffd3047759d0e528a59913263/dada1b1a61dfb11a-92/s1280x1920/4a9c0911f0f0df12cf952e76060b1f14be714fb9.jpg",
+    },
+    varric: {
+        name: "Varric Tethras",
+        avatar_url: "https://static.wikia.nocookie.net/dragonage/images/b/b7/Varric_(The_Veilguard).png",
+    },
+};
+
+module.exports = characters;
